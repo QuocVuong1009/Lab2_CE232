a2f767a3f3cfb7e7bbfcc9bfedd8b85efc516a63
QuocVuong1009 <quocvuong12vn@gmail.com>
2023-04-13 20:44:27 +0700

    CodeLab2

---
+++ i2c_example_main.c
@@ -0,0 +1,258 @@
+/* i2c - Example
+
+   For other examples please check:
+   https://github.com/espressif/esp-idf/tree/master/examples
+
+   See README.md file to get detailed usage of this example.
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include "esp_log.h"
+#include "esp_err.h"
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "font8x8_basic.h"
+#include "ssd1306.h"
+
+static const char *TAG = "i2c-example";
+
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+#define DATA_LENGTH 512                  /*!< Data buffer length of test buffer */
+#define RW_TEST_LENGTH 128               /*!< Data length for r/w test, [0,DATA_LENGTH] */
+#define DELAY_TIME_BETWEEN_ITEMS_MS 1000 /*!< delay time between different test items */
+
+#define I2C_SLAVE_SCL_IO CONFIG_I2C_SLAVE_SCL               /*!< gpio number for i2c slave clock */
+#define I2C_SLAVE_SDA_IO CONFIG_I2C_SLAVE_SDA               /*!< gpio number for i2c slave data */
+#define I2C_SLAVE_NUM I2C_NUMBER(CONFIG_I2C_SLAVE_PORT_NUM) /*!< I2C port number for slave dev */
+#define I2C_SLAVE_TX_BUF_LEN (2 * DATA_LENGTH)              /*!< I2C slave tx buffer size */
+#define I2C_SLAVE_RX_BUF_LEN (2 * DATA_LENGTH)              /*!< I2C slave rx buffer size */
+
+#define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(CONFIG_I2C_MASTER_PORT_NUM) /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQUENCY        /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+
+#define BH1750_SENSOR_ADDR CONFIG_BH1750_ADDR   /*!< slave address for BH1750 sensor */
+#define BH1750_CMD_START CONFIG_BH1750_OPMODE   /*!< Operation mode */
+#define ESP_SLAVE_ADDR CONFIG_I2C_SLAVE_ADDRESS /*!< ESP32 slave address, you can set any 7bit value */
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
+
+SemaphoreHandle_t print_mux = NULL;
+
+/**
+ * @brief Test code to write esp-i2c-slave
+ *        Master device write data to slave(both esp32),
+ *        the data will be stored in slave buffer.
+ *        We can read them out from slave buffer.
+ *
+ * ___________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | write n bytes + ack  | stop |
+ * --------|---------------------------|----------------------|------|
+ *
+ */
+/*static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}*/
+
+/**
+ * @brief i2c master initialization
+ */
+static esp_err_t i2c_master_init(void)
+{
+	int i2c_master_port = 0;
+	i2c_config_t conf = {
+		.mode = I2C_MODE_MASTER,
+		.sda_io_num = 5,
+		.scl_io_num = 4,
+		.sda_pullup_en = GPIO_PULLUP_ENABLE,
+		.scl_pullup_en = GPIO_PULLUP_ENABLE,
+		.master.clk_speed = 100000
+	};
+	i2c_param_config(i2c_master_port, &conf);
+	return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+}
+
+//init OLED
+void ssd1306_init() {
+	esp_err_t espRc;
+
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+	i2c_master_write_byte(cmd, 0x14, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
+	i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
+
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_NORMAL, true);
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_OFF, true);
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		ESP_LOGI(TAG, "OLED configured successfully");
+	} else {
+		ESP_LOGE(TAG, "OLED configuration failed. code: 0x%.2X", espRc);
+	}
+	i2c_cmd_link_delete(cmd);
+}
+
+//hien thi OLED 
+void task_ssd1306_display_text(const void *arg_text) {
+	char *text = (char*)arg_text;
+	uint8_t text_len = strlen(text);
+
+	i2c_cmd_handle_t cmd;
+
+	uint8_t cur_page = 0;
+
+	cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+	i2c_master_write_byte(cmd, 0x00, true); // reset column - choose column --> 0
+	i2c_master_write_byte(cmd, 0x10, true); // reset line - choose line --> 0
+	i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
+
+	i2c_master_stop(cmd);
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	i2c_cmd_link_delete(cmd);
+
+	for (uint8_t i = 0; i < text_len; i++) {
+		if (text[i] == '\n') {
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+			i2c_master_write_byte(cmd, 0x00, true); // reset column
+			i2c_master_write_byte(cmd, 0x10, true);
+			i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
+
+			i2c_master_stop(cmd);
+			i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+			i2c_cmd_link_delete(cmd);
+		} else {
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+			i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
+
+			i2c_master_stop(cmd);
+			i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+			i2c_cmd_link_delete(cmd);
+		}
+	}
+
+	vTaskDelete(NULL);
+}
+
+void display_mf()
+{
+	uint8_t cur_page = 0;
+	uint8_t count = 0;
+	i2c_cmd_handle_t cmd;
+	for (uint8_t i = 0; i < 128; i++) 
+	{
+		count = count + 1;	
+			if (count == 17)	//khi biến count = 17 tức là hàng ngang đã đủ 128 pixel nên chúng ta xuống hàng
+			{
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+			i2c_master_write_byte(cmd, 0x00, true); // reset column
+			i2c_master_write_byte(cmd, 0x10, true);
+			i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
+
+			i2c_master_stop(cmd);
+			i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+			i2c_cmd_link_delete(cmd);
+			count = 0;	//đưa biến count về 0 để bắt đầu lại việc đếm
+			i = i - 1;	//trừ biến i đi 1 giá trị để tránh mất dữ liệu
+			}
+			else
+			{
+			cmd = i2c_cmd_link_create();
+			i2c_master_start(cmd);
+			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+			i2c_master_write(cmd, font8x8_basic_logo1[i], 8, true);
+
+			i2c_master_stop(cmd);
+			i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+			i2c_cmd_link_delete(cmd);
+			}
+	}
+
+}
+
+void task_ssd1306_display_clear(void *ignore) {
+	i2c_cmd_handle_t cmd;
+
+	uint8_t clear[128];
+	for (uint8_t i = 0; i < 128; i++) {
+		clear[i] = 0;
+	}
+	for (uint8_t i = 0; i < 8; i++) {
+		cmd = i2c_cmd_link_create();
+		i2c_master_start(cmd);
+		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+		i2c_master_write_byte(cmd, 0xB0 | i, true);
+
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+		i2c_master_write(cmd, clear, 128, true);
+		i2c_master_stop(cmd);
+		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+		i2c_cmd_link_delete(cmd);
+	}
+
+	vTaskDelete(NULL);
+}
+
+
+void app_main(void)
+{
+    char *text = "20522172\n20522155\n20521819\n20521806";
+    ESP_ERROR_CHECK(i2c_master_init());
+    ssd1306_init();
+	//task_ssd1306_display_clear(text);
+    //task_ssd1306_display_text(text);
+	display_mf();
+    
+}
